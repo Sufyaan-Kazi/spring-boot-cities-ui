a040bcbe2f8c47eff9df335a841c4a1e646e5f87
Sufyaan Kazi <skazi@pivotal.io>
2015-12-29 20:22:16 +0000

    Renamed Test Files

--- src/test/java/io/pivotal/fe/demos/citiesui/CitiesUiApplicationTests.java
+++ src/test/java/io/pivotal/fe/demos/citiesui/CitiesUiApplicationTests.java
@@ -6,12 +6,10 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import io.pivotal.fe.demos.citiesui.SufCitiesUiApplication;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SufCitiesUiApplication.class)
+@SpringApplicationConfiguration(classes = CitiesUiApplication.class)
 @WebAppConfiguration
-public class SufCitiesUiApplicationTests {
+public class CitiesUiApplicationTests {
 
 	@Test
 	public void contextLoads() {
