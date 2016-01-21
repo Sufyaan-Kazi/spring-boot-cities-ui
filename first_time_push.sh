87cb29fbe597d5a85f53075b70a95d3e8608941a
Sufyaan Kazi <skazi@pivotal.io>
2016-01-21 15:00:07 +0000

    Corrected script to use temp placeholder

--- first_time_push.sh
+++ first_time_push.sh
@@ -2,5 +2,5 @@
 ./gradlew build
 cf delete -f cities-ui
 cf delete-service -f citiesService
-cf cups citiesService -p '{"tags":"cities","uri":"http://cities-service-emea.emea.fe.pivotal.io/cities"}'
+cf cups citiesService -p '{"tags":"cities","uri":"http://<insert route and domain here e.g. cities.mydomain.pivotal.io>/cities"}'
 cf push -b java_buildpack_offline
