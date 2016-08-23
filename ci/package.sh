68d5c71cb84c7932d07ca579246218165b738662
Concourse <nobody@concourse.ci>
2016-08-23 16:30:03 +0100

    Test with Callum

--- ci/package.sh
+++ ci/package.sh
@@ -3,9 +3,6 @@
 export TERM=${TERM:-dumb}
 echo "****** Starting"
 cd source-code
-ls
-echo "****** About to build"
 ./gradlew build
-ls build/libs/*.jar
 cp build/libs/*.jar ../build
 echo "****** Finished"
