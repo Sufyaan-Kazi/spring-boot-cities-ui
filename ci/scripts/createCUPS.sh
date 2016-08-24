57fa4886c75121aeb121e1ba53bdf42b103b2723
Concourse <nobody@concourse.ci>
2016-08-24 23:13:35 +0100

    Trying out diff docker images

--- ci/scripts/createCUPS.sh
+++ ci/scripts/createCUPS.sh
@@ -15,7 +15,7 @@ cf_service_delete()
 main()
 {
   SERVICE=citiesService
-  find / -name cf 2>/dev/null
+  /usr/bin/cf
   #install_cli 
   cf_service_delete $SERVICE
   URL=`cf apps | grep cities-service | xargs | cut -d " " -f 6`
