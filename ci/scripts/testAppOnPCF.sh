941a5ece60752dff1983723819e87198fe3f5ce5
Concourse <nobody@concourse.ci>
2016-08-24 14:34:22 +0100

    Playing with Concourse

--- ci/scripts/testAppOnPCF.sh
+++ ci/scripts/testAppOnPCF.sh
@@ -1,16 +1,24 @@
 #!/bin/bash 
+install_cli()
+{
+  curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+  export PATH=.:$PATH
+  cf --version
+  cf login -a $CF_API -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE --skip-ssl-validation
+}
 
-echo $1
+install_cli
 URL=`cf apps | grep cities-ui | xargs | cut -d " " -f 6`
 if [ -z "${URL}" ]
 then
  exit 1
 fi
+echo $1 -> $URL
 
 curl -s $URL | grep $1
 running=`curl -s $URL | grep $1`
-echo $running
 if [ -z "${running}" ]
 then
  exit 1 
 fi
+echo $running
