93f27e30585b7d9fdab48f5d61587c07b9e7009c
Concourse <nobody@concourse.ci>
2016-08-24 14:26:17 +0100

    Playing with Concourse

--- ci/scripts/testAppOnPCF.sh
+++ ci/scripts/testAppOnPCF.sh
@@ -1,9 +1,16 @@
 #!/bin/bash 
-echo $1 $2
-curl -s $1 | grep $2
-running=`curl -s $1 | grep $2`
+
+echo $1
+URL=`cf apps | grep cities-ui | xargs | cut -d " " -f 6`
+if [ -z "${URL}" ]
+then
+ exit 1
+fi
+
+curl -s $URL | grep $1
+running=`curl -s $URL | grep $1`
 echo $running
 if [ -z "${running}" ]
 then
- exit -1 
+ exit 1 
 fi
