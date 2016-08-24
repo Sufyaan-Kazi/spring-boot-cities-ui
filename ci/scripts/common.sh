94903fb67a5ff4d430870da57409471f90e4b421
Concourse <nobody@concourse.ci>
2016-08-24 17:07:15 +0100

    Playing with Concourse

---
+++ ci/scripts/common.sh
@@ -0,0 +1,32 @@
+#!/bin/sh 
+set -e
+
+abort()
+{
+    echo >&2 '
+    ***************
+    *** ABORTED ***
+    ***************
+    '
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+echo_msg()
+{
+  echo ""
+  echo "************** ${1} **************"
+}
+
+install_cli()
+{
+  curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+  export PATH=.:$PATH
+  cf --version
+  cf login -a $api -u $username -p $password -o $organization -s $space --skip-ssl-validation
+}
+
+trap 'abort $LINENO' 0
+SECONDS=0
+SCRIPTNAME=`basename "$0"`
+
--- ci/scripts/deleteApp.sh
+++ ci/scripts/deleteApp.sh
@@ -1,30 +1,8 @@
 #!/bin/sh 
 set -e
-
-abort()
-{
-    echo >&2 '
-    ***************
-    *** ABORTED ***
-    ***************
-    '
-    echo "An error occurred. Exiting..." >&2
-    exit 1
-}
-
-echo_msg()
-{
-  echo ""
-  echo "************** ${1} **************"
-}
-
-install_cli()
-{
-  curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
-  export PATH=.:$PATH
-  cf --version
-  cf login -a $api -u $username -p $password -o $organization -s $space --skip-ssl-validation
-}
+ls
+pwd
+source ./common.sh
 
 cf_app_delete()
 {
