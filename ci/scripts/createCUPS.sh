b22b609401552f2f8aeb15461e784d2f27c526f9
Concourse <nobody@concourse.ci>
2016-08-25 01:04:40 +0100

    Changed check for CUPS

--- ci/scripts/createCUPS.sh
+++ ci/scripts/createCUPS.sh
@@ -2,24 +2,16 @@
 set -e
 . $APPNAME/ci/scripts/common.sh
 
-cf_service_delete()
-{
-  #Delete the Service Instance
-  EXISTS=`cf services | grep ${1} | wc -l | xargs`
-  if [ $EXISTS -ne 0 ]
-  then
-    cf unbind-service $APPNAME ${1}
-    cf delete-service -f ${1}
-  fi
-}
-
 main()
 {
   SERVICE=citiesService
   cf_login 
-  cf_service_delete $SERVICE
-  URL=`cf apps | grep cities-service | xargs | cut -d " " -f 6`
-  cf cups citiesService -p '{"tags":"cities","uri":"http://'"$URL"'/cities"}'
+  EXISTS=`cf services | grep ${1} | wc -l | xargs`
+  if [ $EXISTS -eq 0 ]
+  then
+    URL=`cf apps | grep cities-service | xargs | cut -d " " -f 6`
+    cf cups citiesService -p '{"tags":"cities","uri":"http://'"$URL"'/cities"}'
+  fi
   cf logout
 }
 
