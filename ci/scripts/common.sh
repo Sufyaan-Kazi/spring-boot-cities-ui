5b382cb5af1f90efc53f2a11795d62df3aba595c
Concourse <nobody@concourse.ci>
2016-08-28 15:33:58 +0100

    ssf

--- ci/scripts/common.sh
+++ ci/scripts/common.sh
@@ -28,17 +28,6 @@ summaryOfApps()
   cf apps | tail -n +4
 }
 
-abort()
-{
-    echo >&2 '
-    ***************
-    *** ABORTED ***
-    ***************
-    '
-    echo "An error occurred. Exiting..." >&2
-    exit 1
-}
-
 echo_msg()
 {
   echo ""
@@ -51,3 +40,40 @@ cf_login()
   cf login -a $api -u $username -p $password -o $organization -s $space $ssl
 }
 
+exitIfNull()
+{
+  if [ -z "${1}" ]
+  then
+    echo ${2}
+    exit 1
+  fi
+}
+
+checkAppIsDeployed()
+{
+  cf apps | grep $1 | xargs | cut -d " " -f 6
+  URL=`cf apps | grep $1 | xargs | cut -d " " -f 6`
+  echo "URL is: $URL"
+  exitIfNull $URL
+}
+
+checkSpringBootAppOnPCF()
+{
+  echo_msg "Checking $1 is deployed to PCF and running ok"
+  checkAppIsDeployed $1
+
+  echo_msg "Checking Spring Boot Actuator health endpoint"
+  running=`curl -s $URL/health | grep '"status" : "UP"'`
+  echo $running
+  exitIfNull $running
+}
+
+createVarsBasedOnVersion()
+{
+  #VERSION=`cat resource-version/number | sed -e 's/\./_/g'`
+  VERSION=`cat resource-version/number`
+  #CF_APPNAME=${APPNAME}-${username}-${VERSION}
+  #JARNAME=${APPNAME}-${VERSION}.jar
+
+  echo $VERSION
+}
