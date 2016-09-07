4ba5a4e4d25c8661d3c75b821981750130098ae6
Concourse <nobody@concourse.ci>
2016-09-07 14:47:14 +0100

    Tidied up

--- first_time_push.sh
+++ first_time_push.sh
@@ -3,6 +3,6 @@
 cf delete -f cities-ui
 cf delete-service -f citiesService
 URL=cities-service-appreciational-redness.cfapps.haas-35.pez.pivotal.io
+
 cf cups citiesService -p '{"tags":"cities","uri":"http://'"$URL"'/cities"}'
-BPACK=`cf buildpacks | grep java | grep true | head -n 1 | cut -d ' ' -f1 | xargs`
-cf push -b ${BPACK}
+cf push
