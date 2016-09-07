7dd20371121eae9f32ff618089a476dc55e2d706
Concourse <nobody@concourse.ci>
2016-09-07 14:48:23 +0100

    Windows version of script

---
+++ first_time_push.bat
@@ -0,0 +1,8 @@
+# This push will be slow because we are inserting about 40k city records into the db on initialisation
+.\gradlew build
+cf delete -f cities-ui
+cf delete-service -f citiesService
+URL=cities-service-appreciational-redness.cfapps.haas-35.pez.pivotal.io
+
+cf cups citiesService -p '{"tags":"cities","uri":"http://'"$URL"'/cities"}'
+cf push
