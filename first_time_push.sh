b5273e1f51e95268c3cdfe4d5c7a39bb9eb1061b
Concourse <nobody@concourse.ci>
2016-09-13 00:01:35 +0100

    Tidied

--- first_time_push.sh
+++ first_time_push.sh
@@ -4,8 +4,6 @@ APPNAME=cities-ui
 SI=ServiceReg
 cf unbind-service $APPNAME $SI
 cf delete -f $APPNAME
-#cf delete-service -f $SI
-#cf create-service p-service-registry standard $SI
 cf push -b java_buildpack_offline --no-start --no-route
 echo ""
 echo "Setting environment for SCS"
@@ -14,20 +12,6 @@ echo "Setting environment for SCS"
 CF_TARGET=`cf target | grep "API" | cut -d" " -f5| xargs`
 cf set-env $APPNAME CF_TARGET $CF_TARGET
 
-# Uncomment following lines if not deploying ot the same space as back-end service (i.e. with a SI already there)
-
-# echo ""
-
-# Sleep for service registry
-# max=12
-# for ((i=1; i<=$max; ++i )) ;
-# do
-#  echo "Pausing to allow Service Discovery to Initialise....."
-#  sleep 5
-# done
-
-# Carry on pushing
-echo ""
 echo "Pushing App!"
 BPACK=`cf buildpacks | grep java | grep true | head -n 1 | cut -d ' ' -f1 | xargs`
 cf push -b ${BPACK}
