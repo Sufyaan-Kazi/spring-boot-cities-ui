e99b08d0b1543f6830f6380095e5445ecc4650c9
Sufyaan Kazi <Sufyaan-Kazi@users.noreply.github.com>
2017-04-16 18:53:59 +0100

    sneaky stuff

---
+++ sneaky.sh
@@ -0,0 +1,2 @@
+sleep 20
+cp src/main/java/io/pivotal/fe/demos/citiesui/repository/CityRepository2.txt src/main/java/io/pivotal/fe/demos/citiesui/repository/CityRepository.java
--- v2.sh
+++ v2.sh
@@ -1,3 +1,2 @@
 cp src/main/resources/templates/index2.html src/main/resources/templates/index.html
-sleep 10
-cp src/main/java/io/pivotal/fe/demos/citiesui/repository/CityRepository2.txt src/main/java/io/pivotal/fe/demos/citiesui/repository/CityRepository.java
+sneaky.sh
