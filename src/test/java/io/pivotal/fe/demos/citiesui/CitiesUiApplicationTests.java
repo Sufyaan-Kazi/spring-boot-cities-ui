95e2848302c13c88c0796e3d3e7f5d341c201131
Sufyaan Kazi <sufyaankazi@googlemail.com>
2018-02-25 10:21:22 +0000

    Updated Spring Boot and Tests to 1.5.10

--- src/test/java/io/pivotal/fe/demos/citiesui/CitiesUiApplicationTests.java
+++ src/test/java/io/pivotal/fe/demos/citiesui/CitiesUiApplicationTests.java
@@ -2,13 +2,12 @@ package io.pivotal.fe.demos.citiesui;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CitiesUiApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class CitiesUiApplicationTests {
 
 	@Test
